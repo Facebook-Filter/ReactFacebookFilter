2af967eae46e2ccff724fbb7f58a659a78079646
Abdulkader Freij <abdulkader@arch.localdomain>
2020-02-15 17:01:32 +0200

    add users backend

--- db.js
+++ db.js
@@ -3,6 +3,70 @@ const sqlite= require("sqlite");
 const initializeDB = async () => {
     const db = await sqlite.open("./db.sqlite");
 
+    const getUsers= async()=>{
+        const rows= await db.all('Select * from users');
+        return rows;
+    }
+    
+    const addUser = async props=>{
+        const {username, password} = props;
+        if(!props || !username || !password){
+            throw new Error('you must provide a username and password');
+        }
+        try{
+        const addUser= await db.run(`Insert into users (username, password) values ('${username}', '${password}')`);
+        return addUser.stmt.lastID;
+    }
+    catch(err) {
+        err.message;
+        console.log(err.message);
+        throw new Error('this combination doesnt work');
+    }};
+    
+    const updateUser = async (id,props)=>{
+        const {username, password} = props;
+        console.log("here")
+        if(!props || !props.username && !props.password){
+            throw new Error('you must provide a username and password');
+        }
+        let stmt='';
+        if (username && password){
+            stmt=`update users set username='${username}', password='${password}' where id= ${id}`;
+            console.log(stmt);
+        }
+        else if(username && !password){
+            stmt=`update users set username='${username}' where id= ${id}`;
+        }
+    
+        else if(password && !username){
+            stmt= `update users set password='${password}' where id= ${id}`;
+        }
+    
+        try{
+        const updateUser = await db.run(stmt);
+        console.log(updateUser);
+        if (updateUser.stmt.changes == 0){
+            throw new Error(`users with id ${id} doesnt exist`);
+        }
+    
+        return true;
+    }
+    catch(err) {
+        
+        throw new Error(`Could not update users with id= ${id}` + err);
+    }};
+    
+    const deleteUser = async(id)=>{
+        try{
+        const del= await db.run(`Delete from users where id =${id}`);
+        return del;
+    }
+    catch(err) {
+        err.message;
+        console.log(err.message);
+        throw new Error('this combination doesnt work');
+    }};
+
     const getReviews= async()=>{
         const review= await db.all('Select * from reviews');
         return review;
@@ -46,6 +110,10 @@ const initializeDB = async () => {
         getReviews,
         addReview,
         deleteReview,
+        getUsers,
+        addUser,
+        deleteUser,
+        updateUser,
         getFaq
 
     }
--- index.js
+++ index.js
@@ -8,6 +8,57 @@ const PORT = 5000;
 const start = async () => {
   const controller = await initializeDB();
 
+  app.get("/users", async (req, res) => {
+    const users = await controller.getUsers();
+    res.json(users);
+  });
+
+  app.get("/users/add", async (req, res) => {
+    const username = req.query.username;
+    const password = req.query.password;
+
+    let errors = [];
+    if (username == "" || password == "") {
+      errors.push({
+        status: 403,
+        error: true,
+        message:
+          "you cannot create a user without providing a user name or password"
+      });
+    }
+
+    if (errors.length > 0) {
+      res.json({ status: 403, error: true, message: errors });
+    } else {
+      try {
+        const addUser = await controller.addUser({ username, password });
+        res.json({ id: addUser, username: username, password: password });
+      } catch (e) {
+        res.json({ status: 403, error: true, message: e.message });
+      }
+    }
+  });
+
+  app.get("/users/delete/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+      const del = await controller.deleteUser(id);
+      res.json({ del });
+    } catch (e) {
+      res.json({ status: 403, error: true, message: e.message });
+    }
+  });
+  app.get("/users/update/:id", async (req, res) => {
+    const id = req.params.id;
+    const {username, password}=req.query;
+    try {
+      const updateUser = await controller.updateUser(id,{ username, password });
+      res.json({ updateUser });
+    } catch (e) {
+      res.json({ status: 403, error: true, message: e.message });
+    }
+  });
+
 app.get("/review", async(req, res) => {
   const review = await controller.getReviews();
     res.json(review);
