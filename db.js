ac5a31b2a58cefcff883ba9e345864ac4872f71c
Abdulkader Freij <abdulkader@arch.localdomain>
2020-02-15 16:57:01 +0200

    add reviews backend

--- db.js
+++ db.js
@@ -3,11 +3,53 @@ const sqlite= require("sqlite");
 const initializeDB = async () => {
     const db = await sqlite.open("./db.sqlite");
 
+    const getReviews= async()=>{
+        const review= await db.all('Select * from reviews');
+        return review;
+    }
+
+    const addReview = async props=>{
+        const {username, email, review} = props;
+        if(!props || !username || !email || !review){
+            throw new Error('you must provide a username, email & review');
+        }
+        try{
+        const addReview= await db.run(`Insert into reviews (username, email, review) values ('${username}', '${email}', '${review}')`);
+        return addReview.stmt.lastID;
+    }
+    catch(err) {
+        err.message;
+        console.log(err.message);
+        throw new Error('this combination doesnt work');
+    }};
+ 
+    
+    const deleteReview = async(id)=>{
+        try{
+        const deleteReview= await db.run(`Delete from reviews where id =${id}`);
+        return deleteReview;
+    }
+    catch(err) {
+        err.message;
+        console.log(err.message);
+        throw new Error('this combination doesnt work');
+    }};
+      
+
+    const getFaq= async()=>{
+        const faq= await db.all('Select * from faqs');
+        return faq;
+    }
 
 
+    const controller = {
+        getReviews,
+        addReview,
+        deleteReview,
+        getFaq
 
-
-
+    }
+    return controller;
 
 }
 
--- index.js
+++ index.js
@@ -8,11 +8,52 @@ const PORT = 5000;
 const start = async () => {
   const controller = await initializeDB();
 
-app.get("/", (req, res) => {
-  res.json({ message: "ok" });
+app.get("/review", async(req, res) => {
+  const review = await controller.getReviews();
+    res.json(review);
 });
 
+app.get("/review/add", async (req, res) => {
+  const username = req.query.username;
+  const email = req.query.email;
+  const review=req.query.review;
 
+  let errors = [];
+  if (username == "" || email == "" || review=='') {
+    errors.push({
+      status: 403,
+      error: true,
+      message:
+        "you cannot create a review without providing a username, email and review"
+    });
+  }
+
+  if (errors.length > 0) {
+    res.json({ status: 403, error: true, message: errors });
+  } else {
+    try {
+      const addReview = await controller.addReview({ username, email, review });
+      res.json({ id: addReview, username: username, email: email, review:review });
+    } catch (e) {
+      res.json({ status: 403, error: true, message: e.message });
+    }
+  }
+});
+
+app.get("/review/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const deleteReview = await controller.deleteReview(id);
+    res.json({ deleteReview });
+  } catch (e) {
+    res.json({ status: 403, error: true, message: e.message });
+  }
+});
+
+app.get('/faq', async(req,res)=>{
+  const faq=await controller.getFaq();
+  res.json(faq);
+})
 
 }
 app.listen(PORT, () => 
